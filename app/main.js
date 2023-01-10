576e36776f4c829b20d50de4c16ee4e9735e3461
Semilore Idowu <semijacks27@gmail.com>
2023-01-10 15:30:59 +0100

    respond to multiple PINGs

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ console.log('Logs from your program will appear here!')
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.on('data', () => {
+  connection.on('ping', () => {
     connection.write('+PONG\r\n')
   })
 })
